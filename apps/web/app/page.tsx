3846468cbfcc0be17c966c9a61ec90dcf90d55ea
William Phelps <williamphelps13@gmail.com>
2024-02-25 21:44:09 -0700

    Add daisyui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,7 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-6xl text-cyan-500 underline">Welcome to web!</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   )
 }
