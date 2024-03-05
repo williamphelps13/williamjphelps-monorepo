cda80b06fcdf250404921a6123077aabc24139e5
William Phelps <williamphelps13@gmail.com>
2024-03-04 19:29:56 -0700

    Portfolio: Add placeholder home page

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link'
+
 const Home = () => {
-  return <h1>William Phelps' Portfolio</h1>
+  return (
+    <>
+      <h1>William Phelps' Portfolio</h1>
+      <Link href="/resume">Resume</Link>
+    </>
+  )
 }
 
 export default Home
