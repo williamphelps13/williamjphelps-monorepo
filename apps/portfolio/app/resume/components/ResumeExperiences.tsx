cc34646660684c01075153c6aac7b76245d9887d
William Phelps <williamphelps13@gmail.com>
2024-03-16 17:48:01 -0600

    Portfolio: Add Experiences

--- apps/portfolio/app/resume/components/ResumeExperiences.tsx
+++ apps/portfolio/app/resume/components/ResumeExperiences.tsx
@@ -1,58 +1,53 @@
 import Image from 'next/image'
-import company1Image from 'app/resume/images/company-2.jpg'
-import company2Image from 'app/resume/images/company-2.jpg'
+import { ExperienceData } from 'app/resume/types'
 
-const experiences = [
-  {
-    company: 'Webscale Networks',
-    image: company1Image,
-    link: 'https://www.webscale.com/',
-    location: 'Boulder, Colorado',
-    date: 'Nov 2021 – Present',
-    title: 'Software Engineer',
-    descriptions: [
-      'Webscale is a cloud platform with a mission to streamline or automate the biggest challenges facing e-commerce companies today by empowering them with prescriptive analytics, performance optimizations, and edge security.',
-      'Collaborated with stakeholders, authored requirements, and played a pivotal role in translating designs into robust frontend implementations using Typescript, Svelte, and SCSS, ensuring the delivery of high-quality software.',
-    ],
-  },
-  {
-    company: 'Boulder Wine Merchant',
-    image: company2Image,
-    link: 'https://www.boulderwine.com/',
-    location: 'Boulder, Colorado',
-    date: 'Feb 2017 – March 2021',
-    title: 'Manager, Buyer, and Certified Sommelier',
-    descriptions: [
-      'Developed my leadership skills and ability to drive revenue through team management and customer satisfaction.',
-      'These transferable skills, combined with my technical expertise as a Software Engineer, enable me to deliver exceptional software solutions while prioritizing user satisfaction and business goals.',
-    ],
-  },
-]
-
-const ResumeExperience = () => {
+const ResumeExperience = ({
+  experienceData,
+}: {
+  experienceData: ExperienceData
+}) => {
   return (
-    <section>
-      <h2>Experiences</h2>
-      {experiences.map(
-        ({ company, link, image, location, date, title, descriptions }) => (
-          <article key={`${company}-${date}}`}>
-            <h3>
-              <a href={link} target="_blank" rel="noopener noreferrer">
-                {company}
-              </a>
-            </h3>
-            <Image src={image} alt={`${company}`} width={150} height={150} />
-            <p>{location}</p>
-            <p>{date}</p>
-            <h4>{title}</h4>
-            <ul>
-              {descriptions?.map((description, index) => (
-                <li key={index}>{description}</li>
-              ))}
-            </ul>
-          </article>
-        ),
-      )}
+    <section className="flex flex-col space-y-4 md:space-y-8">
+      <h2 className="section-heading">Experiences</h2>
+      {experienceData.map((experience) => (
+        <article
+          className="space-y-1 md:space-y-2"
+          key={`${experience?.organization}-${experience?.date}}`}
+        >
+          <div className="flex justify-between">
+            <span className="flex gap-4 md:gap-8">
+              <h3 className="tertiary-heading">
+                <a
+                  className="link rounded p-0"
+                  href={experience?.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  title={`${experience?.organization} website`}
+                >
+                  {experience.image && (
+                    <Image
+                      src={experience?.image}
+                      alt={`${experience?.organization} logo`}
+                      height={28}
+                    />
+                  )}
+                  {experience?.organization}
+                </a>
+              </h3>
+              <p className="normal-body leading-7">{experience?.location}</p>
+            </span>
+            <p className="normal-body leading-7">{experience?.date}</p>
+          </div>
+          <h4 className="important-body">{experience?.title}</h4>
+          <ul className="ml-4 list-disc">
+            {experience?.descriptions?.map((description, index) => (
+              <li className="normal-body" key={index}>
+                {description}
+              </li>
+            ))}
+          </ul>
+        </article>
+      ))}
     </section>
   )
 }
