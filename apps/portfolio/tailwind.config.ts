a2c6fd61055b1880c3e780bbbb58e9f6ff97b1d3
William Phelps <williamphelps13@gmail.com>
2024-03-16 17:07:39 -0600

    Portfolio: Add responsive fonts styling and custom fonts

--- apps/portfolio/tailwind.config.ts
+++ apps/portfolio/tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'pop': ['Lobster', 'sans-serif'],
+        'body': ['Calibri', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
