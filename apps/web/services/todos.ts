b37bde54370e6c9903963e78aafaa5563d013023
William Phelps <williamphelps13@gmail.com>
2024-03-16 17:52:33 -0600

    willDoWeb: Add todos service

--- apps/web/services/todos.ts
+++ apps/web/services/todos.ts
@@ -1,5 +1,42 @@
-export const getTodos = async (accessToken: string | undefined) => {
-  const response = await fetch('http://127.0.0.1:8000/', {
+import { NewTodo,Todo } from 'types'
+
+export const getAllTodos = async (accessToken: string | null, search: string) => {
+  const encodedSearch = encodeURIComponent(search);
+  const url = `http://127.0.0.1:8000/todos?search=${encodedSearch}`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+export const getCompletedTodos = async (accessToken: string | null, search: string) => {
+  const encodedSearch = encodeURIComponent(search);
+  const response = await fetch(`http://127.0.0.1:8000/todos/?completed=true&search=${encodedSearch}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+export const getNotCompletedTodos = async (accessToken: string | null, search: string) => {
+  const encodedSearch = encodeURIComponent(search);
+  const response = await fetch(`http://127.0.0.1:8000/todos/?completed=false&search=${encodedSearch}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
@@ -12,3 +49,49 @@ export const getTodos = async (accessToken: string | undefined) => {
 
   return await response.json()
 }
+
+export const updateTodo = async (accessToken: string | null, updatedTodo: Todo) => {
+  const response = await fetch(`http://127.0.0.1:8000/todo/${updatedTodo.id}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedTodo),
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`)
+  }
+}
+
+export const deleteTodo = async (accessToken: string | null, id: number) => {
+  const response = await fetch(`http://127.0.0.1:8000/todo/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`)
+  }
+}
+
+
+export const createTodo = async (accessToken: string | null, newTodo: NewTodo) => {
+  const response = await fetch('http://127.0.0.1:8000/todo', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({...newTodo, complete: false}),
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`)
+  }
+}
+
