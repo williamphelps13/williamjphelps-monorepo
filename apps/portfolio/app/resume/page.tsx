1f8d98ce3dbb9adc2c790f157d550eb349f09384
William Phelps <williamphelps13@gmail.com>
2024-03-16 17:48:32 -0600

    Portfolio: Combine Components

--- apps/portfolio/app/resume/page.tsx
+++ apps/portfolio/app/resume/page.tsx
@@ -2,14 +2,25 @@ import ResumeHeader from 'app/resume/components/ResumeHeader'
 import ResumeEducation from 'app/resume/components/ResumeEducation'
 import ResumeExperience from 'app/resume/components/ResumeExperiences'
 import ResumeTechStack from 'app/resume/components/ResumeTechStack'
+import 'devicon/devicon.min.css'
+
+import {
+  educationData,
+  experienceData,
+  headerData,
+  techStackData,
+} from 'app/resume/data'
 
 const ResumePage = () => {
   return (
-    <main>
-      <ResumeHeader />
-      <ResumeTechStack />
-      <ResumeExperience />
-      <ResumeEducation />
+    <main className="space-y-4 px-8 py-4 md:space-y-8 md:px-16 md:py-12">
+      <ResumeHeader headerData={headerData} />
+      <hr />
+      <ResumeTechStack techStackData={techStackData} />
+      <hr />
+      <ResumeExperience experienceData={experienceData} />
+      <hr />
+      <ResumeEducation educationData={educationData} />
     </main>
   )
 }
