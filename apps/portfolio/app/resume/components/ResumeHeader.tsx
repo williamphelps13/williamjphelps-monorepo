071b4c5aaecbe304aaea92935ce3afeb11adca94
William Phelps <williamphelps13@gmail.com>
2024-03-16 17:46:54 -0600

    Portfolio: Add header

--- apps/portfolio/app/resume/components/ResumeHeader.tsx
+++ apps/portfolio/app/resume/components/ResumeHeader.tsx
@@ -1,57 +1,78 @@
 import Image from 'next/image'
-import headshot from 'app/resume/images/headshot.jpg'
-import locationImage from 'app/resume/images/location.jpg'
+import { HeaderData } from 'app/resume/types'
+import {
+  AtSymbolIcon,
+  ComputerDesktopIcon,
+  HomeModernIcon,
+  PhoneIcon,
+} from '@heroicons/react/24/outline'
+import { Lobster } from 'next/font/google'
 
-const headerData = {
-  fullName: 'William Phelps',
-  headshotImage: headshot,
-  title: 'Software Engineer',
-  location: 'Denver, Colorado',
-  locationImage: locationImage,
-  phone: '7075339586',
-  email: 'williamphelps13@gmail.com',
-  linkedin: 'linkedin.com/in/williamjphelps',
-}
+const lobster = Lobster({ weight: '400', subsets: ['latin'] })
 
-const ResumeHeader = () => {
-  const {
-    fullName,
-    headshotImage,
-    title,
-    location,
-    locationImage,
-    phone,
-    email,
-    linkedin,
-  } = headerData
+const ResumeHeader = ({ headerData }: { headerData: HeaderData }) => {
   return (
-    <header>
-      <h1>{fullName}</h1>
-      <Image
-        src={headshotImage}
-        alt={`${fullName} Headshot`}
-        width={150}
-        height={150}
-      />
-      <h2>{title}</h2>
-      <address>
-        <p aria-label="location">{location}</p>
+    <header className="flex flex-col items-center gap-4 md:flex-row md:items-start md:justify-between md:gap-8">
+      {headerData.headshotImage && (
         <Image
-          src={locationImage}
-          alt={`${location}`}
-          width={150}
-          height={150}
+          alt={`${headerData?.fullName} Headshot`}
+          className="rounded-lg shadow md:hidden"
+          src={headerData?.headshotImage}
+          width={80}
         />
-        <a href={`tel:+1${phone}`}>{phone}</a>
-        <a href={`mailto:${email}`}>{email}</a>
-        <a
-          href={`https://${linkedin}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {linkedin}
-        </a>
-      </address>
+      )}
+      <div className="flex flex-col items-center gap-4 md:items-start md:gap-12">
+        <h1 className={`primary-heading ${lobster.className}`}>
+          {headerData?.fullName}
+        </h1>
+        <h2 className="secondary-heading">{headerData?.title}</h2>
+      </div>
+      <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8">
+        {headerData.headshotImage && (
+          <Image
+            alt={`${headerData?.fullName} Headshot`}
+            className="hidden rounded-xl shadow md:block"
+            src={headerData?.headshotImage}
+            width={160}
+          />
+        )}
+        <address className="important-body flex flex-wrap justify-center gap-1 not-italic md:flex-col md:gap-2">
+          <a
+            aria-label={`Google Map of my location - ${headerData?.location}`}
+            className="link"
+            href="https://maps.app.goo.gl/azE8h8KAhx715SG5A"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <HomeModernIcon className="mr-2 inline h-4 w-4 md:mr-4 md:h-6 md:w-6" />
+            {headerData?.location}
+          </a>
+          <a
+            className="link"
+            href={`tel:+1${headerData?.phone}`}
+          >
+            <PhoneIcon className="mr-2 inline h-4 w-4 md:mr-4 md:h-6 md:w-6" />
+            {headerData?.phone}
+          </a>
+          <a
+            className="link"
+            href={`mailto:${headerData?.email}`}
+          >
+            <ComputerDesktopIcon className="mr-2 inline h-4 w-4 md:mr-4 md:h-6 md:w-6" />
+            {headerData?.email}
+          </a>
+          <a
+            aria-label="Link to my LinkedIn account"
+            className="link"
+            href={`https://${headerData?.linkedin}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AtSymbolIcon className="mr-2 inline h-4 w-4 md:mr-4 md:h-6 md:w-6" />
+            {headerData?.linkedin}
+          </a>
+        </address>
+      </div>
     </header>
   )
 }
