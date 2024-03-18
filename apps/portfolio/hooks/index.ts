0ceb880a6ab3133da17d85fe9e71b0eb210f9f36
William Phelps <williamphelps13@gmail.com>
2024-03-17 21:16:05 -0600

    Portfolio: Add matchMedia check

--- apps/portfolio/hooks/index.ts
+++ apps/portfolio/hooks/index.ts
@@ -3,7 +3,9 @@ import { useEffect, useState } from 'react';
 export type Theme = 'light' | 'dark';
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState<Theme>(() => localStorage.getItem('theme') as Theme || 'light');
+  const [theme, setTheme] = useState<Theme>(() => localStorage.getItem('theme') as Theme ||
+  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+  || 'light');
 
   useEffect(() => {
     const root = document.documentElement;
